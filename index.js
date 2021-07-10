270fa6b2a327623c8c9170f81394d2d504424f1d
Térence Hollander <hollanderterence@gmail.com>
2021-07-10 10:08:41 +0200

    chore: small improvments

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const assert = require('assert');
-const todo = _ => {};
+const noop = _ => {};
 
 /* npm test to start */
 
-/* ex1 (add / at the beginning to do the first exercice)
-// Implem the ex1 function that double even numbers and triple odd ones
-const ex1 = todo;
+/* ex1 (add a / at the beginning of this line to do the first exercice)
+// Implem the ex1 function that doubles even numbers and triple odd ones
+const ex1 = noop;
 assert.deepStrictEqual([1, 2, 3].map(ex1), [3, 4, 9]);
 console.log('🎉 Ex1 OK🎉 ');
 
 /* ex2
-// Implem the ex2 function that do the same thing than ex1 but output in string
-const ex2 = todo;
+// Implem the ex2 function that does the same thing than ex1 but output in string
+const ex2 = noop;
 assert.deepStrictEqual([1, 2, 3].map(ex2), ['3', '4', '9']);
 console.log('🎉 Ex2 OK🎉 ');
 
 /* ex3
 // Implem a pipe function in order to make the test below pass;
-const pipe = () => todo;
+const pipe = () => noop;
 const ex3 = pipe(
   ex1,
   x => x.toString()
